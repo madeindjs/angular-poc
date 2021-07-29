47b883a0bd5ad62612df012f623aa9fec40a7e5b
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2021-07-29 17:06:57 +0200

    wip

--- src/app/users/users-list/users-list.component.ts
+++ src/app/users/users-list/users-list.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { take } from 'rxjs/operators';
 import { AppState } from 'src/app/state.interface';
-import { User, UsersService } from '../../users.service';
+import { User } from '../../users.service';
 
 @Component({
   selector: 'app-users-list',
@@ -13,14 +12,9 @@ import { User, UsersService } from '../../users.service';
 export class UsersListComponent implements OnInit {
   public users: Observable<User[]>;
 
-  constructor(
-    private readonly usersService: UsersService,
-    private readonly store: Store<AppState>
-  ) {}
+  constructor(private readonly store: Store<AppState>) {}
 
   ngOnInit(): void {
-    this.usersService.getUsers().pipe(take(1)).subscribe();
-
     this.users = this.store.select((state) => state.users.users);
   }
 }
--- src/app/users/users-page-index/users-page-index.component.ts
+++ src/app/users/users-page-index/users-page-index.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators';
+import { UsersService } from 'src/app/users.service';
 
 @Component({
   selector: 'app-users-page-index',
   templateUrl: './users-page-index.component.html',
-  styleUrls: ['./users-page-index.component.scss']
+  styleUrls: ['./users-page-index.component.scss'],
 })
 export class UsersPageIndexComponent implements OnInit {
-
-  constructor() { }
+  constructor(private readonly usersService: UsersService) {}
 
   ngOnInit(): void {
+    this.reloadUsers();
   }
 
+  reloadUsers() {
+    this.usersService.getUsers().pipe(take(1)).subscribe();
+  }
 }
