890c23808e3b71ddc8b04140d24f4b8d83e4f85b
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2021-07-29 15:53:14 +0200

    try

--- src/app/users.service.spec.ts
+++ src/app/users.service.spec.ts
@@ -1,18 +1,51 @@
-import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { UsersService } from './users.service';
+import { User, UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [HttpClientTestingModule],
     });
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(UsersService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return expected users', (done) => {
+    const expectedUsers: User[] = [
+      {
+        email: 'toto@toto.fr',
+        firstname: 'toto',
+        lastname: 'toto',
+        username: 'toto',
+        uuid: 'toto',
+      },
+    ];
+
+    service.getUsers().subscribe((users) => {
+      console.log(users);
+      expect(users).toEqual(expectedUsers);
+      done();
+    });
+
+    const req = httpTestingController.expectOne(
+      'https://fakerapi.it/api/v1/users'
+    );
+
+    req.flush(expectedUsers);
   });
 });
