f470179dfe2435596301d57e7b0d076858dcfa3a
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2021-08-06 15:11:00 +0200

    store in localStorage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { StoreModule } from '@ngrx/store';
+import { ActionReducer, MetaReducer, StoreModule } from '@ngrx/store';
+import { localStorageSync } from 'ngrx-store-localstorage';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
@@ -9,13 +10,25 @@ import { loginReducer } from './login/login.reducer';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { usersReducer } from './users.reducer';
 
+export function localStorageSyncReducer(
+  reducer: ActionReducer<any>
+): ActionReducer<any> {
+  return localStorageSync({ keys: ['users', 'login'], rehydrate: true })(
+    reducer
+  );
+}
+const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
+
 @NgModule({
   declarations: [AppComponent, NavBarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    StoreModule.forRoot({ users: usersReducer, login: loginReducer }),
+    StoreModule.forRoot(
+      { users: usersReducer, login: loginReducer },
+      { metaReducers }
+    ),
     LoginModule,
   ],
   providers: [],
